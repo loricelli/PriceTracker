d2639b41f0eb46a56ff09771eda9d013baa52ad0
Lorenzo Ricelli <lorenzo.ricelli@gmail.com>
2016-04-20 11:02:24 +0200

    Riorganizzazione delle pagine

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var bodyParser= require("body-parser");///usr/lib
+//var bodyParser= require("body-parser");///usr/lib
 var fs= require('fs');
 var express= require('express');
 var app=express();
-app.use(bodyParser());
 var ebay = require('ebay-api/index.js');
 var MongoClient = require('mongodb').MongoClient;
 //var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url_mongo = 'mongodb://localhost:27017/test';
-
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({extended: true}));
 //------------------AUX FUNCTIONS-----------------------------------
 function parseUrl(url){
   var parse_url= url.toString().split("/");
@@ -31,28 +31,56 @@ function insertDB(url_mongo,item){
     db.close();
   });
 }
+
+function add_person_to_db(url_mongo,request){
+  var id= request.body.email;
+  var pwd= request.psw1;
+  console.log(id);
+  MongoClient.connect(url_mongo, function(err, db) {
+    db.collection('Person',function(err,collection){
+      if(err) throw err;
+      collection.findOne({_id:id},function(err,document){
+        if(err) console.log(err);
+        console.log(document.pwd);
+      });
+    });
+    db.close();
+  });
+}
 //------------------AUX FUNCTIONS-----------------------------------
 
 app.get('/', function(request,response){ //carica la pagina
-	response.writeHead(200, {"Content-type": "text/html"});
+	//response.writeHead(200, {"Content-type": "text/html"});
 	console.log("main request get");
-	fs.createReadStream("./access_page.html").pipe(response);
+  response.redirect('/access_page');
+  //fs.createReadStream("./access_page.html").pipe(response);
 });
+
+app.get('/index', function(request,response){ //carica la pagina
+	response.writeHead(200, {"Content-type": "text/html"});
+	console.log("index get");
+  //response.redirect('/index');
+  fs.createReadStream("./index.html").pipe(response);
+});
+
 app.get('/register', function(request,response){ //carica la pagina
 	response.writeHead(200, {"Content-type": "text/html"});
-	console.log("main request get");
-	fs.createReadStream("./register.html").pipe(response);
+	console.log("register request get");
+  //response.redirect('/register');
+  fs.createReadStream("./register.html").pipe(response);
 });
+
 app.get('/access_page', function(request,response){ //carica la pagina
 	response.writeHead(200, {"Content-type": "text/html"});
-	console.log("main request get");
-	fs.createReadStream("./access_page.html").pipe(response);
+	console.log("access_page request get");
+  //response.redirect('/access_page');
+  fs.createReadStream("./access_page.html").pipe(response);
 });
 
 
-app.post('/',function(request,response){
+app.post('/index',function(request,response){
 
-	console.log("main request post");
+	console.log("post index");
   var item_id= parseUrl(request.body.url);
 
   ebay.xmlRequest({
@@ -69,7 +97,21 @@ app.post('/',function(request,response){
     console.log("\n");
     console.log("Orario: " + item.Timestamp + "\nArticolo: " + item.Item.Title + "\nPrezzo: " + item.Item.ConvertedCurrentPrice.amount +" €");
   });
-	response.redirect('/');
+	response.redirect('/index');
+});
+
+
+
+app.post('/access_page',function(request,response){
+  console.log("post access page");
+  add_person_to_db(url_mongo,request);
+  response.redirect('/index');
+});
+
+app.post('/register',function(request,response){
+
+    console.log("register post");
+    response.redirect('/index');
 });
 
 app.listen(8080);
